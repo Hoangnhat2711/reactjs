79e65d1a9f8fc84baf431b0ac110cd4b5a03f866
unknown <you@example.com>
2024-09-17 11:53:27 +0700

    titt

---
+++ src/pages/client/ThanhToan.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ThanhToan(props) {
+    return (
+        <div>
+            <h1>Thanh Toan</h1>
+        </div>
+    );
+}
+
+export default ThanhToan;
---
+++ src/pages/client/TimKiem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TimKiem(props) {
+    return (
+        <div>
+            <h1>Tim Kiem</h1>
+        </div>
+    );
+}
+
+export default TimKiem;
